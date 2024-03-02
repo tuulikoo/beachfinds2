784260fecdc2a4562f39e5480cb8e1f8ce50a98a
Tero Kivisaari <tero.kivisaari@paf.com>
2024-03-02 22:43:17 +0200

    fixes

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+//import React from 'react';
 import { LoginForm } from './LoginForm';
 
 export const Login = () => {
--- src/middlewares.ts
+++ src/middlewares.ts
@@ -3,7 +3,7 @@ import {NextFunction, Request, Response} from 'express';
 import CustomError from './classes/customError';
 import {ErrorResponse} from './types/MessageTypes';
 
-const notFound = (req: Request, res: Response, next: NextFunction) => {
+const notFound = (req: Request, _res: Response, next: NextFunction) => {
   const error = new CustomError(`🔍 - Not Found - ${req.originalUrl}`, 404);
   next(error);
 };
@@ -12,8 +12,9 @@ const errorHandler = (
   err: CustomError,
   req: Request,
   res: Response<ErrorResponse>,
-  next: NextFunction,
+  _next: NextFunction,
 ) => {
+  console.log("Req: " + req.baseUrl)
   console.error('errorHandler', err.message);
   res.status(err.status || 500);
   res.json({
