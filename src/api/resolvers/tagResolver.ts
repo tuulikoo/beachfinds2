91a42093459d4dbfc567baf83af9353bd6c53715
tuulikoo <tuuli.kivisaari@gmail.com>
2024-03-05 11:46:40 +0200

    tags

--- src/api/resolvers/tagResolver.ts
+++ src/api/resolvers/tagResolver.ts
@@ -2,17 +2,18 @@ import tagModel from '../models/tagModel';
 
 export const tagResolver = {
   Query: {
-    // If you need to fetch tags directly, you can add those queries here.
-  },
+    allTags: async () => {return await tagModel.find();},
+    tagById: async (_parent: undefined, { id }: { id: string }) => {
+      return await tagModel.findById(id);
+    }, // <--- Added missing closing bracket
+  }, // <--- Added missing closing bracket
+
   Mutation: {
     createTag: async (_parent: undefined, { label }: { label: string }) => {
       return await tagModel.create({ label });
     },
-    updateTag: async (_parent: undefined, { id, label }: { id: string; label: string }) => {
-      return await tagModel.findByIdAndUpdate(id, { label }, { new: true });
-    },
     deleteTag: async (_parent: undefined, { id }: { id: string }) => {
       return await tagModel.findByIdAndDelete(id);
-    },
-  },
+    }
+  }
 };
