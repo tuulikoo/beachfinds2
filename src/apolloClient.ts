d2569ac512f933690386d6e66feca7b957f2855d
tuulikoo <tuuli.kivisaari@gmail.com>
2024-03-14 09:14:01 +0200

    upd url to apollo

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
 
-const httpLink = new HttpLink({ uri: 'http://localhost:3000/graphql' });
+const httpLink = new HttpLink({ uri: 'tuulikiv-beachfinds.azurewebsites.net/graphql' });
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem('token');
