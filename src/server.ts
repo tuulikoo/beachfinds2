fa8c490c18291d01c8bc51904378a8af6f2703de
tuulikoo <tuuli.kivisaari@gmail.com>
2024-03-14 10:19:51 +0200

    cors for all requests

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
 app.use(express.static('.'));
 app.use(express.json());
-app.use(cors({ origin: "https://polite-ground-086db8903.4.azurestaticapps.net" }));
+app.use(cors());
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 const mongoConnect = async () => {
